b0219b53d4ca65b82705768d80a0bb36358b9dcd
Jeffrey Britts <jbritts@magento.com>
2017-03-20 15:42:27 -0400

    js override for quote price rounding

---
+++ view/adminhtml/requirejs-config.js
@@ -0,0 +1,7 @@
+var config = {
+    "map": {
+        "*": {
+            "Magento_NegotiableQuote/quote/create/negotiated-price": "MagentoEse_B2bAdminConfigurations/js/negotiated-price"
+        }
+    }
+}
