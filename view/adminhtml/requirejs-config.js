82a998d42ce29cc6919183c4675abf9b6aa74990
Jeffrey Britts <jbritts@magento.com>
2017-06-07 15:02:55 -0400

    no longer need js override for quote prices

--- view/adminhtml/requirejs-config.js
+++
@@ -1,7 +0,0 @@
-var config = {
-    "map": {
-        "*": {
-            "Magento_NegotiableQuote/quote/create/negotiated-price": "MagentoEse_B2bAdminConfigurations/js/negotiated-price"
-        }
-    }
-}
